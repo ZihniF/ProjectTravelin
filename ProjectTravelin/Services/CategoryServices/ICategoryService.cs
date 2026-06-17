4ea96b771c7357d9a4a5c3a2d42904865e2608aa
Z.G <Z.G@DESKTOP-O7S7PCN>
2026-06-17 16:12:08 +0300

    Proje dosyası ekle.

---
+++ ProjectTravelin/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using Project3Travelin.Dtos.CategoryDtos;
+
+namespace Project3Travelin.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetCategoryByIdDto> GetCategoryByIdAsync(string id);
+
+
+    }
+}
